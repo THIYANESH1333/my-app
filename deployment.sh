7c01cd71c50a6a422db8677d4376b83d80817abd
THIYANESH1333 <127740258+THIYANESH1333@users.noreply.github.com>
2025-04-05 06:22:56 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -7,10 +7,11 @@ docker login -u thiyanesh1333 -p THIY@2003
 echo "THIY@2003" | docker login -u "thiyanesh1333" --password-stdin
 
 # Tag and push the image
-docker tag thiyanesh1333/react-app
+docker tag thiyanesh1333/react-app thiyanesh1333/react-app:latest
 
-docker push thiyanesh1333/react-app
+docker push thiyanesh1333/react-app:latest
 
-docker pull thiyanesh1333/react-app
+# Run the Container
+docker run -d -p 80:80 thiyanesh1333/react-app:latest
 
 docker run -d -p 8000:80 --name=my-service-repo thiyanesh1333/react-app
