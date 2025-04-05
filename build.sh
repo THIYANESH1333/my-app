f7230ccf3bb4d56093840853294022f2b1b79764
THIYANESH1333 <127740258+THIYANESH1333@users.noreply.github.com>
2025-04-05 06:45:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-docker build -t thiyanesh1333/react-app .
+docker build -t task37 .
+echo Hyyy
+docker login -u thiyanesh1333 -p THIY@2003
+docker tag task37 thiyanesh1333/react-app
+docker push thiyanesh1333/react-app
+kubectl apply -f  deploy.yaml --validate=false
+kubectl apply -f svc.yaml --validate=false
